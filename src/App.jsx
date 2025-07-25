9cc7275662c5d8a648d4bde6b12a55718526d746
eren iridere <eiridere@gmail.com>
2025-07-26 02:19:33 +0300

    Add responsive design components and styling with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
   return (
-    <main>
-      <div className="bg-red-500">
-        <h1 className="text-3xl font-bold underline">Welcome to the app</h1>
-      </div>
-    </main>
+    <div>
+      <Hero />
+    </div>
   );
 };
 
