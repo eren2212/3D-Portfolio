362d5e2f74d90e81cfeef7c3efd26f41a3d94f15
eren iridere <eiridere@gmail.com>
2025-07-26 01:05:31 +0300

    tailwindcss add to project and configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <main>
-      <div>
-        <h1>Welcome to the app</h1>
+      <div className="bg-red-500">
+        <h1 className="text-3xl font-bold underline">Welcome to the app</h1>
       </div>
     </main>
   );
